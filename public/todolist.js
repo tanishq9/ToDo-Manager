8322f38fb85d2ab5eb81ff4f337a0fb3bd6a5a21
Tanishq Saluja <tanishqsaluja18@gmail.com>
2019-01-28 23:51:13 +0530

    first blood

---
+++ public/todolist.js
@@ -0,0 +1,43 @@
+// We will write all of UI our code , inside the below function as this function only works
+// after the whole UI has been loaded
+// We are confident about the fact that the html page has been loaded , all the elements are fully available
+
+$(function(){
+    // Inside $ use the CSS selector
+    let newtodobox = $('#newtodo');
+    let addtodo = $('#addtodo');
+    let todolist = $('#todolist');
+
+    addtodo.click(function(){
+        console.log('Button was clicked.')
+        // If you are not using jquery function then you cannot use 
+        // .val() as all these functions are added by jquery
+        let newtodo = newtodobox.val();
+        console.log(newtodo);
+
+        // AJAX : Async Javascrsipt and XML
+        // async means w/o reloading the page, the page can
+        // still make get and post request to the server
+        // by just interacting with the server and DOESNT reload the page
+        $.post(
+            // We are sending request using AJAX from frontend to backend and then show data back to the frontend
+            '/todos', // jis link pe post request bhejni hai
+            {task:newtodo},// we receive this data in req.body.task ,check todos.js for clarification
+            function(data){ 
+                // in routes/todos.js,check in post request,we are sending the whole todo list as response
+                // this is the callback function which is called after we receive the response , which is the data
+                // this data is an array of all the tasks whose size keeps on increasing
+                // or this is an array of all the tasks that we have posted to server till now
+                console.log(data);
+                todolist.empty();
+                for(todo of data){
+                    todolist.append('<li>' + todo.task + '</li>');
+                }
+            }
+        );
+    });
+    
+    console.log("Page is loaded.");
+})
+console.log("Some stuff.")
+// append , empty are jquery functions
